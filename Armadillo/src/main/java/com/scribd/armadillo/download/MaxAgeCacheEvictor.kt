988b73357d4dabab2c578c8cdeda0d2952da052d
Nathan Sass <nathans@scribd.com>
2021-07-27 15:12:37 -0400

    updated MaxAgeCacheEvictor

--- Armadillo/src/main/java/com/scribd/armadillo/download/MaxAgeCacheEvictor.kt
+++ Armadillo/src/main/java/com/scribd/armadillo/download/MaxAgeCacheEvictor.kt
@@ -5,29 +5,46 @@ import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.upstream.cache.Cache
 import com.google.android.exoplayer2.upstream.cache.CacheEvictor
 import com.google.android.exoplayer2.upstream.cache.CacheSpan
+import java.util.LinkedList
+import java.util.Queue
 
 /**
- * This cache evictor ensures that cached content older then maxAgeMillis is not served.
- *
- * Future improvements of this evictor should:
- * - have a more performant oldestCacheSpan method
- * - also introduce LRU behavior
+ * This cache evictor will:
+ *  - preferentially remove content that is older then [contentMaxAgeMillis]
+ *  - then function as an lru cache for valid content
+ *  - if [Int.MAX_VALUE] is passed for [contentMaxAgeMillis], this cache will function as an LRU cache
  */
-internal class MaxAgeCacheEvictor(private val maxBytes: Long,
-                                  private val contentMaxAgeMillis: Int = CONTENT_MAX_AGE_MILLIS,
-                                  private val clock: Clock = object : Clock {
-                                      override fun currentTimeMillis(): Long = System.currentTimeMillis()
-                                  }) : CacheEvictor {
+internal class MaxAgeCacheEvictor(
+    private val maxBytes: Long,
+    private val contentMaxAgeMillis: Int = CONTENT_MAX_AGE_MILLIS,
+    private val clock: Clock = object : Clock {
+        override fun currentTimeMillis(): Long = System.currentTimeMillis()
+    },
+) : CacheEvictor {
     @VisibleForTesting
     internal interface Clock {
         fun currentTimeMillis(): Long
     }
 
     @VisibleForTesting
-    internal val contentAges = mutableMapOf<Int, CachePair>()
+    internal data class CacheSpanExpiration(val key: Int, val expiration: Long)
 
     @VisibleForTesting
-    internal data class CachePair(var cacheSpan: CacheSpan, val expiration: Long)
+    internal val content = mutableMapOf<Int, CacheSpan>()
+
+    /**
+     * This structure will track how long a span has been cached for. This should only be modified when a span is added or removed. An
+     * updated span should not trigger any changes here. The OS often moves around [CacheSpan]s internally to optimize storage, these
+     * updates do not correspond to refreshes with the network and therefore should not modify the expiration times.
+     */
+    @VisibleForTesting
+    internal val expirations : Queue<CacheSpanExpiration> = LinkedList()
+
+    /**
+     * This structure will handle lru ordering
+     */
+    @VisibleForTesting
+    internal val lruArr = LinkedList<Int>()
 
     @VisibleForTesting
     internal var currentSize: Long = 0
@@ -48,20 +65,32 @@ internal class MaxAgeCacheEvictor(private val maxBytes: Long,
         }
     }
 
+
     override fun onSpanAdded(cache: Cache, span: CacheSpan) {
         val key = span.key()
-        if (contentAges.containsKey(key)) {
-            val previous = contentAges[key]!!
-            contentAges[key] = previous.copy(cacheSpan = span)
-        } else {
-            contentAges[key] = CachePair(span, expirationTimestamp())
+        if (!content.containsKey(key)) {
+            expirations.add(CacheSpanExpiration(key, expirationTimestamp()))
             currentSize += span.length
         }
+
+        lruArr.remove(key)
+        lruArr.add(key)
+        content[key] = span
         evictCache(cache, 0)
     }
 
+    /**
+     * This triggered by calls in [evictCache] to [Cache.removeSpan]
+     *
+     * It is expected that this method will return because all the work is already done in [evictCache].
+     * [expirations] is not handled here as it is already handled in [evictCache]. If the api changes and this
+     * method gets called at other times, discrepancies will not cause issues.
+     */
     override fun onSpanRemoved(cache: Cache, span: CacheSpan) {
-        contentAges.remove(span.key())
+        val key = span.key()
+        content[key] ?: return
+        content.remove(key)
+        lruArr.remove(key)
         currentSize -= span.length
     }
 
@@ -71,29 +100,39 @@ internal class MaxAgeCacheEvictor(private val maxBytes: Long,
 
     private fun evictCache(cache: Cache, requiredSpace: Long) {
 
-        var oldestSpan = oldestCacheSpan()
+        // remove expired content
+        while (expirations.peek()?.expiration?.isExpired() == true) {
 
-        while (oldestSpan?.isExpired() == true) {
-            cache.removeSpan(oldestSpan.cacheSpan)
+            val expiredWrapper = expirations.poll() ?: continue
+            val key = expiredWrapper.key
+            val expiredSpan = content[key] ?: continue
 
-            oldestSpan = oldestCacheSpan()
+            lruArr.remove(key)
+            content.remove(key)
+            currentSize -= expiredSpan.length
+
+            /**
+             * Triggers [onSpanRemoved]
+             */
+            cache.removeSpan(expiredSpan)
         }
 
-        while (currentSize + requiredSpace > maxBytes && oldestSpan != null) {
-            oldestSpan.cacheSpan.let {
-                cache.removeSpan(it)
-            }
-            oldestSpan = oldestCacheSpan()
+        // LRU behavior
+        while (currentSize + requiredSpace > maxBytes && lruArr.peekLast() != null) {
+            val key = lruArr.pollLast() ?: break
+            val cacheSpan = content[key] ?: break
+            currentSize -= cacheSpan.length
+
+            /**
+             * Triggers [onSpanRemoved]
+             */
+            cache.removeSpan(cacheSpan)
         }
     }
 
-    @VisibleForTesting
-    internal fun oldestCacheSpan(): CachePair? = contentAges.minByOrNull { it.value.expiration }?.value
-
     private fun expirationTimestamp() = clock.currentTimeMillis() + contentMaxAgeMillis
 
-    private fun CachePair.isExpired(): Boolean = clock.currentTimeMillis() > expiration
-
+    private fun Long.isExpired(): Boolean = clock.currentTimeMillis() > this
 }
 
 @VisibleForTesting
--- Armadillo/src/test/java/com/scribd/armadillo/download/MaxAgeCacheEvictorTest.kt
+++ Armadillo/src/test/java/com/scribd/armadillo/download/MaxAgeCacheEvictorTest.kt
@@ -2,11 +2,10 @@ package com.scribd.armadillo.download
 
 import com.google.android.exoplayer2.upstream.cache.Cache
 import com.google.android.exoplayer2.upstream.cache.CacheSpan
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.mock
 
 class MaxAgeCacheEvictorTest {
 
@@ -17,13 +16,15 @@ class MaxAgeCacheEvictorTest {
     private companion object {
         const val MAX_BYTES = 10L
         const val CONTENT_MAX_AGE_MILLIS = 10
-        const val CURRENT_TIME_MILLIS = 1000L
     }
+
+    private var currentTimeMillis = 1000L
 
     @Before
     fun setUp() {
-        clock = mock()
-        whenever(clock.currentTimeMillis()).thenReturn(CURRENT_TIME_MILLIS)
+        clock = object : MaxAgeCacheEvictor.Clock {
+            override fun currentTimeMillis(): Long = currentTimeMillis
+        }
         cache = mock()
         cacheEvictor = MaxAgeCacheEvictor(MAX_BYTES, CONTENT_MAX_AGE_MILLIS, clock)
     }
@@ -32,27 +33,28 @@ class MaxAgeCacheEvictorTest {
     fun onSpanRemoved_cacheIsFull_removesSpanDecrementSize() {
         val cacheSpan = CacheSpan("cool", 0, 3L)
         val key = cacheSpan.key()
-        cacheEvictor.contentAges[key] = MaxAgeCacheEvictor.CachePair(cacheSpan, CURRENT_TIME_MILLIS)
+        cacheEvictor.content[key] = cacheSpan
         cacheEvictor.currentSize = MAX_BYTES
 
         cacheEvictor.onSpanRemoved(cache, cacheSpan)
 
-        assertThat(cacheEvictor.contentAges.containsKey(key)).isFalse
+        assertThat(cacheEvictor.content.containsKey(key)).isFalse
         assertThat(cacheEvictor.currentSize).isEqualTo(7)
     }
 
     @Test
     fun onSpanAdded_cacheMiss_addsKey() {
         // Initially it is all empty
-        assertThat(cacheEvictor.contentAges.isEmpty()).isTrue
+        assertThat(cacheEvictor.content.isEmpty()).isTrue
+        assertThat(cacheEvictor.expirations.isEmpty()).isTrue
         assertThat(cacheEvictor.currentSize).isEqualTo(0L)
         // Make the call
         val cacheSpan = CacheSpan("cool", 0, 3L)
         val key = cacheSpan.key()
         cacheEvictor.onSpanAdded(cache, cacheSpan)
         // Verify Item is added
-        val expectedCachePair = MaxAgeCacheEvictor.CachePair(cacheSpan, CURRENT_TIME_MILLIS + CONTENT_MAX_AGE_MILLIS)
-        assertThat(cacheEvictor.contentAges[key]).isEqualTo(expectedCachePair)
+        assertThat(cacheEvictor.content[key]).isEqualTo(cacheSpan)
+        assertThat(cacheEvictor.expirations.poll()).isEqualTo(MaxAgeCacheEvictor.CacheSpanExpiration(key, currentTimeMillis + CONTENT_MAX_AGE_MILLIS))
         assertThat(cacheEvictor.currentSize).isEqualTo(3L)
     }
 
@@ -60,43 +62,45 @@ class MaxAgeCacheEvictorTest {
     fun onSpanAdded_cacheHit_updatesValue() {
         // Set up contentAges with a piece of content
         val cacheSpan = CacheSpan("cool", 0, 3L)
-        val key = cacheSpan.key()
-        val initialPair = MaxAgeCacheEvictor.CachePair(cacheSpan, CURRENT_TIME_MILLIS + CONTENT_MAX_AGE_MILLIS)
-        cacheEvictor.contentAges[key] = initialPair
-        cacheEvictor.currentSize = 3L
+        cacheEvictor.onSpanAdded(cache, cacheSpan)
+
         // Make a new span with the same position & length
         val updatedCacheSpan = CacheSpan("cool2", 0, 3L)
+
         // Make the call
-        cacheEvictor.onSpanAdded(cache, updatedCacheSpan)
+        cacheEvictor.onSpanTouched(cache, cacheSpan, updatedCacheSpan)
         // Verify item is updated
-        assertThat(cacheEvictor.contentAges.size).isEqualTo(1)
-        assertThat(cacheEvictor.contentAges[updatedCacheSpan.key()]!!.cacheSpan).isEqualTo(updatedCacheSpan)
+        assertThat(cacheEvictor.content.size).isEqualTo(1)
+        assertThat(cacheEvictor.content[updatedCacheSpan.key()]).isEqualTo(updatedCacheSpan)
+        val expectedExpiration = MaxAgeCacheEvictor.CacheSpanExpiration(
+            updatedCacheSpan.key(),
+            currentTimeMillis + CONTENT_MAX_AGE_MILLIS)
+        assertThat(cacheEvictor.expirations.poll()).isEqualTo(expectedExpiration)
         assertThat(cacheEvictor.currentSize).isEqualTo(3L)
     }
 
     @Test
-    fun evictCache_removesExpiredContentOnly() {
+    fun onSpanAdded_removesExpiredContentOnly() {
         // Setup content
-        val expiredContentSpan1 = CacheSpan("expired1", 0, 3L)
-        val expiredContent1 = MaxAgeCacheEvictor.CachePair(expiredContentSpan1, CURRENT_TIME_MILLIS - 1)
+        val spanToExpire1 = CacheSpan("expired1", 0, 3L)
+        cacheEvictor.onSpanAdded(cache, spanToExpire1)
 
-        val expiredContentSpan2 = CacheSpan("expired2", 3, 3L)
-        val expiredContent2 = MaxAgeCacheEvictor.CachePair(expiredContentSpan2, CURRENT_TIME_MILLIS - 2)
+        val spanToExpire2 = CacheSpan("expired2", 3, 3L)
+        cacheEvictor.onSpanAdded(cache, spanToExpire2)
 
-        val notExpiredContentSpan = CacheSpan("valid", 6, 3L)
-        val notExpiredContent = MaxAgeCacheEvictor.CachePair(notExpiredContentSpan, CURRENT_TIME_MILLIS + 1)
+        assertThat(cacheEvictor.expirations.size).isEqualTo(2)
+        assertThat(cacheEvictor.content.size).isEqualTo(2)
 
-        cacheEvictor.contentAges[expiredContentSpan1.key()] = expiredContent1
-        cacheEvictor.contentAges[expiredContentSpan2.key()] = expiredContent2
-        cacheEvictor.contentAges[notExpiredContentSpan.key()] = notExpiredContent
+        // Advance time so previous content is expired
+        currentTimeMillis += CONTENT_MAX_AGE_MILLIS + 1
 
-        // Make the call & Assert
-        assertThat(cacheEvictor.oldestCacheSpan()).isEqualTo(expiredContent2)
-        cacheEvictor.contentAges.remove(expiredContentSpan2.key())
+        // Make the call
+        val validContent = CacheSpan("valid", 6, 3L)
+        cacheEvictor.onSpanAdded(cache, validContent)
 
-        assertThat(cacheEvictor.oldestCacheSpan()).isEqualTo(expiredContent1)
-        cacheEvictor.contentAges.remove(expiredContentSpan1.key())
-
-        assertThat(cacheEvictor.oldestCacheSpan()).isEqualTo(notExpiredContent)
+        assertThat(cacheEvictor.content.size).isEqualTo(1)
+        assertThat(cacheEvictor.content[validContent.key()]).isEqualTo(validContent)
+        assertThat(cacheEvictor.expirations.size).isEqualTo(1)
+        assertThat(cacheEvictor.lruArr.size).isEqualTo(1)
     }
 }
